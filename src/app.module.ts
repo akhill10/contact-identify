efc2685a49704f8d15a3cc5b20b8271cef678d42
Akhil <pawanakhil.dasari@gmail.com>
2023-07-04 17:55:37 +0000

    feat: added rate limiting for apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from '../ormconfig';
 import { ConfigModule } from '@nestjs/config';
 import { ContactModule } from './modules/contact/contact.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(AppDataSource.options),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ContactModule,
   ],
   controllers: [],
